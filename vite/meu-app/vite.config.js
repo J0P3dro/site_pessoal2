5e17d97953c93838f74f59f9faa009abf998474c
J0P3dro <jpagalhardo@gmail.com>
2024-05-12 14:57:52 -0300

    TESTE 3

--- vite/meu-app/vite.config.js
+++ vite/meu-app/vite.config.js
@@ -1,12 +1,17 @@
 // @ts-check
 import reactPlugin from 'vite-plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 /**
  * @type { import('vite').UserConfig }
  */
 const config = {
-  jsx: 'react',
-  plugins: [reactPlugin]
+  plugins: [reactPlugin, reactRefresh()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-router-dom']
+    }
+  }
 }
 
 export default config
